833bfab62859edeb12fbbfae09a7153d48600354
tj705 <woojin598@nate.com>
2023-08-24 12:47:36 +0900

    member test2

--- src/main/java/kr/co/tj/memberservice/MemberServiceApplication.java
+++ src/main/java/kr/co/tj/memberservice/MemberServiceApplication.java
@@ -1,6 +1,7 @@
 package kr.co.tj.memberservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
+@SpringBootConfiguration
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
